0f1e495fa684a4b7db484e0042d4d8f499479bc7
Ant29bj <bojorquez29an@gamil.com>
2023-10-09 14:46:27 -0700

    generic service agregado

--- src/app/payroll/payroll.entity.ts
+++ src/app/payroll/payroll.entity.ts
@@ -3,7 +3,7 @@ import { GenericEntity } from '../generics/generic.entity';
 
 @Entity('payroll')
 export class Payroll extends GenericEntity {
-  // se debe de relacionar con un a tabla de empleados
+  // se debe de relacionar con un a tabla de empleados (o se debe de relacionar con usuarios aun no se sabe)
   @Column()
   emmitedFor: string;
 

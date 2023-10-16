bcb8b17e3fa73e106a2ac322466eee8b6a676951
fasterbarry <noris.kelvin@gmail.com>
2023-10-16 12:51:47 -0700

    ahora si ya quedo

--- src/app/empleados/empleados.controller.ts
+++ src/app/empleados/empleados.controller.ts
@@ -2,33 +2,25 @@ import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common'
 import { EmpleadosService } from './empleados.service';
 import { EmpleadosEntity } from './empleados.entity';
 import { CreateEmpleadoDto } from './dto/create-empleado.dto';
+import { ApiBody } from '@nestjs/swagger';
+import { Entity } from 'typeorm';
+import { GenericController } from '../generics/generic.controller';
 
 @Controller('empleados')
-export class EmpleadosController {
-  constructor(private readonly empleadosService: EmpleadosService) {}
+export class EmpleadosController extends GenericController<EmpleadosEntity, EmpleadosService>{
+  constructor(private readonly empleadosService: EmpleadosService) {super(empleadosService)}
 
-  @Get()
-  findAll(): Promise<EmpleadosEntity[]> {
-    return this.empleadosService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: number): Promise<EmpleadosEntity> {
-    return this.empleadosService.findOne(id);
-  }
-
+  @ApiBody({type: CreateEmpleadoDto, required: true})
   @Post()
-  create(@Body() empleado: CreateEmpleadoDto): Promise<EmpleadosEntity> {
+  override async create(@Body() empleado: EmpleadosEntity){
     return this.empleadosService.create(empleado);
   }
 
+  @ApiBody({type: CreateEmpleadoDto, required: true})
   @Put(':id')
-  update(@Param('id') id: number, @Body() empleado: EmpleadosEntity): Promise<EmpleadosEntity> {
+  override async update(@Param('id') id: number, @Body() empleado: EmpleadosEntity) {
     return this.empleadosService.update(id, empleado);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string): Promise<void> {
-    return this.empleadosService.remove(id);
-  }
+  
 }

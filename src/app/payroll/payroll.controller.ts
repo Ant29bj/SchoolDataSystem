9a7c261d17c2005f49cea36c7bcd3fd1d07777c2
Antonio B <bojorquez29an@gmail.com>
2023-10-16 07:18:07 -0700

    controlador generico agregado, metodo find(options agregado)

--- src/app/payroll/payroll.controller.ts
+++ src/app/payroll/payroll.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { PayrrollService } from './payroll.service';
+import { Payroll } from './payroll.entity';
+import { GenericController } from '../generics/generic.controller';
 
-@Controller('Payroll')
-export class PayrollController {
-  constructor(private readonly payrollService: PayrrollService) {}
-
-  @Get()
-  getPayrolls() {
-    return this.payrollService.find();
+@Controller('payroll')
+export class PayrollController extends GenericController<Payroll, PayrrollService> {
+  constructor(private readonly payrollService: PayrrollService) {
+    super(payrollService);
   }
+
 }

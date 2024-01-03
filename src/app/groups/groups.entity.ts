cd90b3b1cdb165549edf6b56473f0502508a80d7
C3j45 <luislugo1e@gmail.com>
2024-01-02 23:16:04 -0700

    Merge branch 'sekai' into cejas

--- src/app/groups/groups.entity.ts
+++ src/app/groups/groups.entity.ts
@@ -4,25 +4,19 @@ import {Studetns} from '../students/students.entity';
 
 @Entity()
 export class GroupsEntity extends GenericEntity {
->>>>>>> 2c898e8 (tablas creadas falta relaciones)
+
   @Column({ type: 'time' })
   schedule: string;
   // name = teacher + horario agregar funcion en el servicio
   @Column()
   name: string;
 
-<<<<<<< HEAD
+
   @ManyToOne(() => TeachersEntity, (teacher) => teacher.id)
   teacher: TeachersEntity;
 
   @OneToMany(() => StudentsEntity, (student) => student.group)
   students: StudentsEntity[];
-=======
-  // relacion ManyToOne
-  @Column()
-  teacher: string;
 
-  // relacion OneToMany;
-  @Column()
-  students: string;
+
 }

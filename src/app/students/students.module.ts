3d786094996c4dd0fac8eaa4da06708a9b14e8fd
danielniebla <82241926+danielniebla@users.noreply.github.com>
2024-04-12 20:17:00 -0700

    importar parents module

--- src/app/students/students.module.ts
+++ src/app/students/students.module.ts
@@ -3,9 +3,10 @@ import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudentsEntity } from './students.entity';
+import { ParentsModule } from '../parents/parents.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([StudentsEntity])],
+  imports: [TypeOrmModule.forFeature([StudentsEntity]), ParentsModule],
   controllers: [StudentsController],
   providers: [StudentsService],
   exports: [StudentsService],

24e323db03fc496b94b2f3d183e1ed6c23198df6
danieniebla <82241926+danielniebla@users.noreply.github.com>
2024-06-23 17:50:39 -0700

    grade students_group

--- src/app/students_groups/students_groups.entity.ts
+++ src/app/students_groups/students_groups.entity.ts
@@ -1,11 +1,17 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 import { GroupsEntity } from '../groups/groups.entity';
+import { Max, Min } from 'class-validator';
 import { StudentsEntity } from '../students/students.entity';
 
 @Entity()
 export class StudentsGroupsEntity {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Min(0)
+  @Max(10)
+  @Column()
+  grade: number;
 
   @ManyToOne(() => StudentsEntity, student => student.studentGroups)
   student: StudentsEntity;
